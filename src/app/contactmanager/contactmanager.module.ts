6e672aabd3cabf8ae4657d16c3dfa3ce1200a1c7
Michał Raźny <michal.razny23@gmail.com>
2022-01-13 21:06:09 +0100

    feat(table): add table with pagination, filtering and sorting

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { UserService } from './services/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NotesComponent } from './components/notes/notes.component';
 
 const routes: Routes = [
   {
@@ -37,7 +38,8 @@ const routes: Routes = [
     ContactmanagerAppComponent,
     ToolbarComponent,
     MainContentComponent,
-    SidenavComponent
+    SidenavComponent,
+    NotesComponent
   ],
   imports: [
     CommonModule,
